37cd214ff5a36fe5a22513e3a5e72739ccde0103
abcsde <ruysahu1393@gmail.com>
2024-02-22 16:33:28 +0530

    liveweb

--- websws/arya/public/js/main.js
+++ websws/arya/public/js/main.js
@@ -1,21 +1,20 @@
 // intersection observer
-const chngClr = (entries) => {
-    entries.forEach((entry) => {
-      const intersecting = entry.isIntersecting;
-      // entry.target.style.backgroundColor = intersecting ? "blue" : "red";
-      if (intersecting) {
-        document.getElementById("navbar_top").className += " white_top ";
-      } else {
-        document.getElementById("navbar_top").classList.remove("white_top");
-      }
-    });
-  };
-  const observer = new IntersectionObserver(chngClr, { threshold: 0.75 });
-  const observer2 = new IntersectionObserver(chngClr, { threshold: 0.75 });
-  const observer3 = new IntersectionObserver(chngClr, { threshold: 0.75 });
-  observer.observe(document.getElementById("products"));
-  observer2.observe(document.getElementById("intro"));
-  observer3.observe(document.getElementById("prod_desc"));
+// const chngClr = (entries) => {
+//     entries.forEach((entry) => {
+//       const intersecting = entry.isIntersecting;
+//       if (intersecting) {
+//         document.getElementById("navbar_top").className += " white_top ";
+//       } else {
+//         document.getElementById("navbar_top").classList.remove("white_top");
+//       }
+//     });
+//   };
+//   const observer = new IntersectionObserver(chngClr, { threshold: 0.75 });
+//   const observer2 = new IntersectionObserver(chngClr, { threshold: 0.75 });
+//   const observer3 = new IntersectionObserver(chngClr, { threshold: 0.75 });
+//   observer.observe(document.getElementById("products"));
+//   observer2.observe(document.getElementById("intro"));
+//   observer3.observe(document.getElementById("prod_desc"));
 
 
 
